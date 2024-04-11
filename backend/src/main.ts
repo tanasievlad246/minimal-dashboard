a330f6436667bb533293c65b03a5f911191ec97e
tanasievlad246 <tanasievladcristian@gmail.com>
2024-04-12 00:16:12 +0300

    setup for dockercompose completed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { BackendModule } from './backend.module';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(BackendModule);
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
