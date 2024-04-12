54a99a03021acc630112e38091efc663b1ff59e2
tanasievlad246 <tanasievladcristian@gmail.com>
2024-04-13 00:35:38 +0300

    Finished navigation with some extra views

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -6,9 +6,13 @@ import { useEffect, useState } from "react";
 export const Navigation = () => {
     const location = useLocation();
     const pathSegments = location.pathname.split('/').filter(segment => segment !== '');
-    const [currentPath, setCurrentPath] = useState(`${pathSegments[0][0].toUpperCase()}${pathSegments[0].slice(1).toLowerCase()}`);
-    
+    const [currentPath, setCurrentPath] = useState(``);
+
     useEffect(() => {
+        if (pathSegments.length === 0) {
+            setCurrentPath('Home');
+            return;
+        }
         setCurrentPath(`${pathSegments[0][0].toUpperCase()}${pathSegments[0].slice(1).toLowerCase()}`);
     }, [location]);
 
@@ -19,25 +23,25 @@ export const Navigation = () => {
         },
         {
             name: 'Bills',
-            path: '#'
+            path: '/bills'
         },
         {
             name: 'Expenses',
-            path: '#'
+            path: '/expenses'
         },
         {
             name: 'Reports',
-            path: '#'
+            path: '/reports'
         }
     ]
-    return <div className="flex flex-col col-span-2 items-center text-slate-600 text-xl px-10 py-5">
+    return <div className="flex flex-col h-full col-span-2 items-center text-slate-600 text-xl px-10 py-5">
         <div className="flex items-center justify-center font-extrabold text-slate-400 bg-white p-4 rounded-lg w-full h-20 text-center">
             <h1>LOGO</h1>
         </div>
         <div className="my-10 self-start w-full">
             <div className="justify-self-start">Menu</div>
             <div className="flex flex-col w-full p-4">
-                <Link to="/" className="flex w-full items-center justify-between p-2 hover:bg-primary-500 rounded-lg">
+                <Link to="/invoices" className="flex w-full items-center justify-between p-2 hover:bg-primary-500 rounded-lg">
                     <span>Home</span>
                 </Link>
                 {menuItems.map((item, index) => {
@@ -48,5 +52,6 @@ export const Navigation = () => {
                 })}
             </div>
         </div>
+
     </div>
 }
