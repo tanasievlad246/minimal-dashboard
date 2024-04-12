e8a33ef18e174389101da6b62b3a0325a7021d1e
tanasievlad246 <tanasievladcristian@gmail.com>
2024-04-13 00:36:27 +0300

    extra views added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,9 @@ import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { checkUserIsAuthenticated } from "./features/user-authentication";
 import useAuthCheck from "./hooks/checkAuth";
 import { Navigation } from "./components/Navigation";
+import { Bills } from "./views/bills";
+import { Expenses } from "./views/expenses";
+import { Reports } from "./views/reports";
 
 function App() {
   useAuthCheck();
@@ -26,7 +29,7 @@ function App() {
   }, [value]);
 
   return (
-    <div className="grid grid-flow-col align-middle grid-cols-12 bg-gradient-to-b from-primary-100 to-primary-600 h-screen p-4">
+    <div className="grid grid-flow-col align-middle grid-cols-12 bg-gradient-to-b from-primary-100 to-primary-600 h-screen p-4 overflow-hidden">
       <Navigation />
       <Routes>
         <Route path="/" element={<Login />} />
@@ -36,6 +39,21 @@ function App() {
           </ProtectedRoute>
         } />
         {/* Catch all routes */}
+        <Route path="/bills" element={
+          <ProtectedRoute>
+            <Bills />
+          </ProtectedRoute>
+        } />
+        <Route path="/expenses" element={
+          <ProtectedRoute>
+            <Expenses />
+          </ProtectedRoute>
+        } />
+        <Route path="/Reports" element={
+          <ProtectedRoute>
+            <Reports />
+          </ProtectedRoute>
+        } />
         <Route path="*" element={<Login />} />
       </Routes>
     </div>
