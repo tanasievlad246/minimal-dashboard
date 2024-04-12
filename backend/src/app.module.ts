8c575c1f2b4d8abd5ab771604867bb50902ccdb4
tanasievlad246 <tanasievladcristian@gmail.com>
2024-04-12 10:01:15 +0300

    Added prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InvoicesModule } from './invoices/invoices.module';
 import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [InvoicesModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
